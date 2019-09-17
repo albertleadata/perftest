ed4c6b493a35bcb713802c5c1663ebfe0c3ba11e
Matt Samudio <matt@samudio.net>
2019-09-17 00:35:14 -0500

    Corrected timbrado host

--- src/test/scala/timbrado/BluejaySimulation.scala
+++ src/test/scala/timbrado/BluejaySimulation.scala
@@ -21,7 +21,7 @@ class BluejaySimulation extends Simulation {
 		"Accept" -> "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
 		"Upgrade-Insecure-Requests" -> "1")
 
-	val uri2 = "http://canarydev:8080/timbrado/portal"
+	val uri2 = "http://timbrado:8080/timbrado/portal"
 
 	val scn = scenario("BluejaySimulation")
 		.exec(http("alpha")
